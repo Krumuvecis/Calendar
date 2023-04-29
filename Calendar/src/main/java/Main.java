88081bfb26fb5002d4a315967b2518cce48d1f6b
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2023-04-29 04:25:07 +0300

    0.1

---
+++ Calendar/src/main/java/Main.java
@@ -0,0 +1,7 @@
+// a placeholder class
+
+public class Main {
+    public static void main(String[] args) {
+        //do stuff here
+    }
+}
---
+++ Calendar/src/test/java/MainTest.java
@@ -0,0 +1,7 @@
+// a placeholder class
+
+public class MainTest {
+    public static void main(String[] args) {
+        //do stuff here
+    }
+}
